8ec8e194f778d2429c376c2f3d7d1fc545783388
Your Name <99066591+ArtemCherepakhin@users.noreply.github.com>
2023-04-09 17:05:39 +0300

    News_Portal

---
+++ News_Portal/NewsPaper/news/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import PostsList, PostDetail, PostCreate, PostUpdate, PostDelete
+
+urlpatterns = [
+   # Path — означает путь.
+   # В данном случае путь ко всем товарам у нас останется пустым,
+   # чуть позже станет ясно почему.
+   # Т.к. наше объявленное представление является классом,
+   # а Django ожидает функцию, нам надо представить этот класс в виде view.
+   # Для этого вызываем метод as_view.
+   path('', PostsList.as_view(), name='post_list'),
+   path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+   path('create/', PostCreate.as_view(), name='post_create'),
+   path('<int:pk>/update/', PostUpdate.as_view(), name='post_update'),
+   path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
+]
